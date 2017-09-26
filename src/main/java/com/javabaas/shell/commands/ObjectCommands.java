f1692e7b73fe8531ccca12c86d34d976e375ef56
zangyilin <112408136qq@gmail.com>
2017-09-26 17:30:25 +0800

    java sdk

--- src/main/java/com/javabaas/shell/commands/ObjectCommands.java
+++ src/main/java/com/javabaas/shell/commands/ObjectCommands.java
@@ -1,13 +1,11 @@
 package com.javabaas.shell.commands;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javabaas.javasdk.JBField;
 import com.javabaas.javasdk.JBObject;
+import com.javabaas.javasdk.JBQuery;
 import com.javabaas.javasdk.JBUtils;
 import com.javabaas.shell.common.CommandContext;
-import com.javabaas.shell.entity.JBSField;
-import com.javabaas.shell.entity.JBSObject;
-import com.javabaas.shell.entity.JBSimpleResult;
 import com.javabaas.shell.util.DateUtil;
 import com.javabaas.shell.util.FieldUtil;
 import com.javabaas.shell.util.PropertiesUtil;
@@ -26,7 +24,6 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -58,7 +55,8 @@ public class ObjectCommands implements CommandMarker {
         String className = context.getCurrentClass();
         try {
             Map<String, Object> map = JBUtils.readValue(string, Map.class);
-            JBObject object = JBUtils.parseObjectFromMap(map);
+            JBObject object = new JBObject(className);
+            JBUtils.copyPropertiesFromMapToJBObject(object, map);
             object.save();
             System.out.println(Ansi.ansi().fg(Ansi.Color.GREEN).a("Object added.").reset());
         } catch (HttpClientErrorException e) {
@@ -75,11 +73,12 @@ public class ObjectCommands implements CommandMarker {
             System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("No object!").reset());
             return;
         }
-        String id = inputs[0];
-        String object = inputs[1];
-        String className = context.getCurrentClass();
         try {
-            rest.put(properties.getHost() + "object/" + className + "/" + id, object);
+            String id = inputs[0];
+            String className = context.getCurrentClass();
+            JBObject object = JBObject.createWithOutData(className, id);
+            JBUtils.copyPropertiesFromMapToJBObject(object, JBUtils.readValue(inputs[1], Map.class));
+            object.save();
             System.out.println(Ansi.ansi().fg(Ansi.Color.GREEN).a("Object updated.").reset());
         } catch (HttpClientErrorException e) {
             System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a(e.getResponseBodyAsString()).reset());
@@ -91,8 +90,9 @@ public class ObjectCommands implements CommandMarker {
             throws JsonProcessingException {
         context.cancelDoubleCheck();
         String className = context.getCurrentClass();
-        String result = rest.getForObject(properties.getHost() + "object/" + className + "/" + id, String.class);
-        System.out.println(result);
+        JBQuery query = new JBQuery(className);
+        JBObject object = query.get(id);
+        System.out.println(object);
     }
 
     @CliCommand(value = "list", help = "Show objects in class.")
@@ -100,20 +100,17 @@ public class ObjectCommands implements CommandMarker {
                      @CliOption(key = {"skip"}, mandatory = false, unspecifiedDefaultValue = "0", specifiedDefaultValue = "0") final String skip)
             throws JsonProcessingException {
         context.cancelDoubleCheck();
-        ObjectMapper mapper = new ObjectMapper();
-        String className = context.getCurrentClass();
-        String url = properties.getHost() + "object/" + className + "?where={where}";
-        Map<String, Object> params = new HashMap<>();
-        params.put("where", where);
-        params.put("skip", skip);
-        if (skip != null) {
-            params.put("skip", skip);
-        }
         try {
-            JBSObject[] result = rest.getForObject(url, JBSObject[].class, params);
-            for (JBSObject baasObject : result) {
-                System.out.println(mapper.writeValueAsString(baasObject));
+            String className = context.getCurrentClass();
+            JBQuery query = new JBQuery(className);
+            if (!JBUtils.isEmpty(skip)) {
+                query.setSkip(Integer.parseInt(skip));
             }
+            if (!JBUtils.isEmpty(where)) {
+                query.setWhereSting(where);
+            }
+            List<JBObject> list = query.find();
+            list.forEach(object -> System.out.println(object));
         } catch (HttpClientErrorException e) {
             System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a(e.getResponseBodyAsString()).reset());
         }
@@ -128,19 +125,23 @@ public class ObjectCommands implements CommandMarker {
                       @CliOption(key = {"s"}, mandatory = false, unspecifiedDefaultValue = "0", specifiedDefaultValue = "1") final String single)
             throws JsonProcessingException {
         context.cancelDoubleCheck();
-        String className = context.getCurrentClass();
-        String url = properties.getHost() + "object/" + className + "?where={where}&skip={skip}";
-        Map<String, Object> params = new HashMap<>();
-        params.put("where", where);
-        params.put("skip", skip);
         try {
+            String className = context.getCurrentClass();
+            JBQuery query = new JBQuery(className);
+            if (!JBUtils.isEmpty(skip)) {
+                query.setSkip(Integer.parseInt(skip));
+            }
+            if (!JBUtils.isEmpty(where)) {
+                query.setWhereSting(where);
+            }
+            List<JBObject> list = query.find();
+
             //创建表格
             V2_AsciiTable at = new V2_AsciiTable();
             at.addRule();
             //控制列宽度
             WidthFixedColumns width = new WidthFixedColumns();
-            JBSObject[] result = rest.getForObject(url, JBSObject[].class, params);
-            JBSField[] fields = rest.getForObject(properties.getHost() + "master/clazz/" + className + "/field", JBSField[].class);
+            List<JBField> fields = JBField.list(className);
             //整理表头
             List<Object> headers = new LinkedList<>();
             List<Object> types = new LinkedList<>();
@@ -152,76 +153,57 @@ public class ObjectCommands implements CommandMarker {
                 headers.add("createdAt");
                 types.add("<DATE>");
                 width.add(21);
-                headers.add("updatedAt");
-                types.add("<DATE>");
-                width.add(21);
+//                headers.add("updatedAt");
+//                types.add("<DATE>");
+//                width.add(21);
             }
-            if (plat.equals("1")) {
-                //显示平台
-                headers.add("createdPlat");
-                types.add("<STRING>");
-                width.add(13);
-                headers.add("updatedPlat");
-                types.add("<STRING>");
-                width.add(13);
-            }
-            if (acl.equals("1")) {
-                headers.add("ACL");
-                types.add("<ACL>");
-                width.add(20);
-            }
+//            if (plat.equals("1")) {
+//                //显示平台
+//                headers.add("createdPlat");
+//                types.add("<STRING>");
+//                width.add(13);
+//                headers.add("updatedPlat");
+//                types.add("<STRING>");
+//                width.add(13);
+//            }
+//            if (acl.equals("1")) {
+//                headers.add("ACL");
+//                types.add("<ACL>");
+//                width.add(20);
+//            }
             //自定义字段
-            for (JBSField field : fields) {
+            fields.forEach(field -> {
                 headers.add(field.getName());
                 types.add(FieldUtil.getFieldType(field.getType()));
                 width.add(20);
-            }
+            });
             at.addRow(headers.toArray());
             at.addRow(types.toArray());
             at.addStrongRule();
-            for (JBSObject baasObject : result) {
+            list.forEach(baasObject -> {
                 List<Object> cols = new LinkedList<>();
-                cols.add(baasObject.get("_id"));
-                for (String key : baasObject.keySet()) {
-                    Object value = baasObject.get(key);
-                    if (value != null) {
-                        switch (key) {
-                            case "createdAt":
-                            case "updatedAt":
-                                if (time.equals("1")) {
-                                    //显示时间
-                                    cols.add(DateUtil.format(Long.valueOf(value.toString())));
-                                }
-                                break;
-                            case "createdPlat":
-                            case "updatedPlat":
-                                if (plat.equals("1")) {
-                                    //显示平台
-                                    cols.add(value);
-                                }
-                                break;
-                            case "getACL":
-                                if (acl.equals("1")) {
-                                    //显示ACL
-                                    cols.add(value);
-                                }
-                                break;
-                        }
-                    } else {
-                        cols.add("");
-                    }
+                cols.add(baasObject.getObjectId());
+                if (time.equals("1")) {
+                    cols.add(DateUtil.format(Long.valueOf(baasObject.getCreatedAt())));
                 }
-                for (JBSField field : fields) {
+                if (plat.equals("1")) {
+                    cols.add("");
+                }
+//                if (acl.equals("1")) {
+//                    cols.add(baasObject.getAcl());
+//                }
+
+                fields.forEach(field -> {
                     Object value = baasObject.get(field.getName());
                     if (value == null) {
                         cols.add("");
                     } else {
                         cols.add(value);
                     }
-                }
+                });
                 at.addRow(cols.toArray());
                 at.addRule();
-            }
+            });
             AsciiTableRenderer rend = new AsciiTableRenderer();
             rend.setTheme(V2_E_TableThemes.UTF_LIGHT.get());
             rend.setWidth(width);
@@ -248,7 +230,8 @@ public class ObjectCommands implements CommandMarker {
         context.cancelDoubleCheck();
         String className = context.getCurrentClass();
         try {
-            rest.delete(properties.getHost() + "object/" + className + "/" + id);
+            JBObject object = JBObject.createWithOutData(className, id);
+            object.delete();
             System.out.println(Ansi.ansi().fg(Ansi.Color.GREEN).a("Object deleted.").reset());
         } catch (HttpClientErrorException e) {
             System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a(e.getResponseBodyAsString()).reset());
@@ -270,13 +253,12 @@ public class ObjectCommands implements CommandMarker {
     public void count(@CliOption(key = {""}, mandatory = false, help = "Query condition.") final String where)
             throws JsonProcessingException {
         context.cancelDoubleCheck();
-        String className = context.getCurrentClass();
-        String url = properties.getHost() + "object/" + className + "/count?where={where}";
-        Map<String, Object> params = new HashMap<>();
-        params.put("where", where);
         try {
-            JBSimpleResult result = rest.getForObject(url, JBSimpleResult.class, params);
-            System.out.println(result.getData("count"));
+            String className = context.getCurrentClass();
+            JBQuery query = new JBQuery(className);
+            query.setWhereSting(where);
+            int count = query.count();
+            System.out.println(count);
         } catch (HttpClientErrorException e) {
             System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a(e.getResponseBodyAsString()).reset());
         }
